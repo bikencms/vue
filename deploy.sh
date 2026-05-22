d75cc526bd7ff9e609367cc4b45b815375d8f0ee
Minh Pham <johndoe@example.com>
2026-05-22 15:13:28 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ pnpm build
 pm2 restart ecosystem.config.js --watch
 
 #clear frontend cache
-curl -X POST -i -H "x-nuxt-multi-cache-token: O5ilxqx5k1ZzFMjEVr" http://127.0.0.1:3000/__nuxt_multi_cache/purge/all
+curl -X POST -i -H "x-nuxt-multi-cache-token: O5ilxqx5k1ZzFMjEVr" http://localhost:3000/__nuxt_multi_cache/purge/all
 
 #kill current process
 fuser -k 3000/tcp
